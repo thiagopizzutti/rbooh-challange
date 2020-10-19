4ebc4c334c2399e99b53d4f4497cd248da9ccf98
Thiago Pizzutti <tpizzutti@gmail.com>
2020-10-19 15:34:12 -0300

    feat: new EditButton Component

---
+++ src/components/EditButton/index.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import AddToPhotosRoundedIcon from '@material-ui/icons/AddToPhotosRounded';
+import MuiButton from '@material-ui/core/Button';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import {ButtonProps, Typography} from '@material-ui/core'
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    margin: {
+      marginLeft: theme.spacing(1)
+    }
+  }),
+);
+
+
+const Button: React.FC<ButtonProps> = ({onClick, ...rest}) => {
+  const classes = useStyles();
+
+  return (
+    
+      <MuiButton
+      variant="contained"
+      color="inherit"
+      style={{
+        color: "#4d4d4d",
+        fontSize: "10px",
+        fontWeight: "bold",
+        
+      }}
+      onClick={onClick}
+      {...rest}
+      
+    
+    ><Typography
+    variant="h4"
+      >
+        Salvar Prato
+    </Typography>
+        <AddToPhotosRoundedIcon
+          fontSize="large"              
+          className={classes.margin}/>
+      </MuiButton> 
+
+    
+    
+  
+  );
+};
+
+export default Button;
